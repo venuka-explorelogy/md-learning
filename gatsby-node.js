0ea0a65b2671cfc746826a0a46e31fbe7ba65fc9
venuka <venuka@explorelogy.lk>
2021-03-04 19:17:14 +0530

    config and gatsby-node addedforauto slug

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,39 +1,153 @@
-exports.createPages = async ({ actions, graphql, reporter }) => {
-    const { createPage } = actions
+/**
+ * Implement Gatsby's Node APIs in this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/node-apis/
+ */
 
-    const blogPostTemplate = require.resolve(`./src/templates/blog-post.js`)
+// You can delete this file if you're not using it
+const path = require("path")
+const { createFilePath } = require(`gatsby-source-filesystem`)
 
-    const result = await graphql(`
+// exports.createPages = ({ actions, graphql }) => {
+//   const { createPage } = actions
+
+//   return graphql(`
+//     {
+//       allMarkdownRemark {
+//         edges {
+//           node {
+//             html
+//             id
+//             frontmatter {
+//               path
+//             }
+//           }
+//         }
+//       }
+//     }
+//   `).then(res => {
+//     if (res.errors) {
+//       return Promise.reject(res.errors)
+//     }
+//     res.data.allMarkdownRemark.edges.forEach(({ node }) => {
+//       createPage({
+//         path: node.frontmatter.path,
+//         component: postTemplate,
+//       })
+//     })
+//   })
+// }
+
+exports.createPages = async ({ graphql, actions, reporter }) => {
+  const { createPage } = actions
+
+  // Define a template for blog post
+
+  const postTemplate = path.resolve(`${__dirname}/src/templates/blog-post.js`)
+
+  // Get all markdown blog posts sorted by date
+  const result = await graphql(
+    `
       {
         allMarkdownRemark(
-          sort: { order: DESC, fields: [frontmatter___date] }
+          sort: { fields: [frontmatter___date], order: ASC }
           limit: 1000
         ) {
-          edges {
-            node {
-              frontmatter {
-                slug
-              }
+          nodes {
+            id
+            fields {
+              slug
             }
           }
         }
-      } 
-    `)
+      }
+    `
+  )
 
-    // Handle errors
-    if (result.errors) {
-        reporter.panicOnBuild(`Error while running GraphQL query.`)
-        return
+  if (result.errors) {
+    reporter.panicOnBuild(
+      `There was an error loading your blog posts`,
+      result.errors
+    )
+    return
+  }
+
+  const posts = result.data.allMarkdownRemark.nodes
+
+  // Create blog posts pages
+  // But only if there's at least one markdown file found at "content/blog" (defined in gatsby-config.js)
+  // `context` is available in the template as a prop and as a variable in GraphQL
+
+  if (posts.length > 0) {
+    posts.forEach((post, index) => {
+      const previousPostId = index === 0 ? null : posts[index - 1].id
+      const nextPostId = index === posts.length - 1 ? null : posts[index + 1].id
+
+      createPage({
+        path: post.fields.slug,
+        component: postTemplate,
+        context: {
+          id: post.id,
+          previousPostId,
+          nextPostId,
+        },
+      })
+    })
+  }
+}
+
+exports.onCreateNode = ({ node, actions, getNode }) => {
+  const { createNodeField } = actions
+  if (node.internal.type === `MarkdownRemark`) {
+    const value = createFilePath({ node, getNode })
+    createNodeField({
+      name: `slug`,
+      node,
+      value,
+    })
+  }
+}
+
+exports.createSchemaCustomization = ({ actions }) => {
+  const { createTypes } = actions
+
+  // Explicitly define the siteMetadata {} object
+  // This way those will always be defined even if removed from gatsby-config.js
+
+  // Also explicitly define the Markdown frontmatter
+  // This way the "MarkdownRemark" queries will return `null` even when no
+  // blog posts are stored inside "content/blog" instead of returning an error
+  createTypes(`
+    type SiteSiteMetadata {
+      siteUrl: String
+      social: Social
+    }
+  
+    type Social {
+      twitter: String
+    }
+    type MarkdownRemark implements Node {
+      frontmatter: Frontmatter
+      fields: Fields
+    }
+    type Frontmatter {
+      title: String
+      description: String
+      date: Date @dateformat
+    }
+    type Fields {
+      slug: String
+
     }
 
-    result.data.allMarkdownRemark.edges.forEach(({ node }) => {
-        createPage({
-            path: node.frontmatter.slug,
-            component: blogPostTemplate,
-            context: {
-                // additional data can be passed via context
-                slug: node.frontmatter.slug,
-            },
-        })
-    })
+  `)
 }
+
+// exports.onCreateWebpackConfig = ({ actions }) => {
+//   const { setWebpackConfig } = actions
+//   setWebpackConfig({
+//     externals: {
+//       jquery: "jQuery", // important: 'Q' capitalized
+//     },
+//   })
+// }
